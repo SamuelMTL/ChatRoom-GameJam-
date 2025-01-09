29b613207e12c524abe25548591771711a169c9b
SamuelMTL <samuel.mtl11@gmail.com>
2025-01-09 19:36:15 -0300

    adicionando o sistema de escolhas

--- chatroom-gamejam2024/scripts/tela_chat.gd
+++ chatroom-gamejam2024/scripts/tela_chat.gd
@@ -1,6 +1,9 @@
 extends Control
 
-@export_file("res://Dialogue/json/textoChat1Intro.json") var json_file: String = "" 
+@export_file("res://Dialogue/json/textoChat1Intro.json") var json_file1: String = "" 
+@export_file("res://Dialogue/json/textoChat1Op1.json") var json_file2: String = ""
+@export_file("res://Dialogue/json/textoChat1Op2.json") var json_file3: String = ""
+@export_file("res://Dialogue/json/textoChat1Fim.json") var json_file4: String = ""
 
 var dialogue = []
 var current_dialogue_id = -1
@@ -13,14 +16,25 @@ func _ready():
 	start()
 
 func start():
-	dialogue = load_dialogue()
+	dialogue = load_dialogue(json_file1)
 	
-func load_dialogue():
+func load_dialogue(json_file: String):
 	var json_as_text = FileAccess.get_file_as_string(json_file)
 	var json_as_dict = JSON.parse_string(json_as_text)
 	
 	return json_as_dict
 		
+		
+func switch_dialogue(choice: int):
+	if choice == 1:
+		dialogue = load_dialogue(json_file2)
+	elif choice == 2:
+		dialogue = load_dialogue(json_file3)
+	else:
+		dialogue = load_dialogue(json_file4)
+		
+	current_dialogue_id = -1
+	next_script()
 		
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
