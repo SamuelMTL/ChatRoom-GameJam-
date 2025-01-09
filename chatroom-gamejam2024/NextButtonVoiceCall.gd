be4f304071919a5dae07c04d84a67abafeaadb5e
SamuelMTL <samuel.mtl11@gmail.com>
2025-01-08 22:52:16 -0300

    fluxo de telas e tela de conclusao

---
+++ chatroom-gamejam2024/NextButtonVoiceCall.gd
@@ -0,0 +1,13 @@
+extends Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.connect("pressed", self._button_pressed)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _button_pressed():
+	get_tree().change_scene_to_file("res://TelaRecap2.tscn")
